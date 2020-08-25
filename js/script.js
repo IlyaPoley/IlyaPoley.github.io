d21ed94f782cb333dfa22603482907715a657ab6
Ilya <52371671+IlyaPoley@users.noreply.github.com>
2020-08-25 13:37:53 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -95,6 +95,22 @@ function toggleSlide(item) {
 
 toggleSlide('.jobs-item__link');
 toggleSlide('.jobs-item__back');
+    
+   /*заполнение калы прогресса*/
+$('.skill-per').each(function(){
+    var $this = $(this);
+    var per = $this.attr('per');
+    $this.css("width",per+'%');
+    $({animatedValue: 0}).animate({animatedValue: per}, {
+      duration: 1000,
+      step: function() {
+        $this.attr('per', Math.floor(this.animatedValue) + '%');
+      },
+      complete: function() {
+        $this.attr('per', Math.floor(this.animatedValue) + '%');
+      },
+    });
+  });
 
     new WOW().init();
 });
