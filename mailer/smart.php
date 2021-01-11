5a7e1c2674e1b5101beb9e643bda6534d365cb13
Ilya <52371671+IlyaPoley@users.noreply.github.com>
2021-01-11 19:06:58 +0600

    Update smart.php

--- mailer/smart.php
+++ mailer/smart.php
@@ -16,7 +16,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'ilya-polei@mail.ru';                 // Наш логин
-$mail->Password = 'fokfu2-cenZab-kymmoj';                           // Наш пароль от ящика
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
